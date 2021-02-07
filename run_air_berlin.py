6a5569bba91a8c976650fbcadc88af76c9c8e4d1
moslehm <moslehm@mymacewan.ca>
2021-02-06 19:15:08 -0700

    Modify run_air_berlin.py to work with AB Solution Checker

--- run_air_berlin.py
+++ run_air_berlin.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
 from air_berlin import *
+import sys
 
 
-if __name__ == "__main__":
-    X = input("Large disks:\n")
-    x = input("Small disks:\n")
+def main():
+    # X = input("Large disks:\n")
+    # x = input("Small disks:\n")
+    with open("AB.sample") as f:
+        X = f.readline()
+        x = f.readline()
 
     X = np.array([int(disc) for disc in X.split(" ")], dtype=np.int32)
     x = np.array([[int(disc) for disc in x.split(" ")]], dtype=np.int8)
@@ -11,14 +16,19 @@ if __name__ == "__main__":
     n = int(np.sqrt(X.shape[0] - 1))
 
     heuristic = PairwiseDistanceHeuristic(n, scale=(1 / (8 * (n - 1))))
-    fringe = MinMaxFringe(5)
+    fringe = Fringe()
     expanded = Expanded()
 
-    path = search(X, x, heuristic, fringe, expanded)
+    path = search(X, x, heuristic, fringe, expanded, verbose=False)
 
+    f = open(sys.argv[2], "w")
     if path is None:
-        print("No solution")
+        f.write("No solution\n")
     else:
-        print("Solution is")
+        f.write("Solution is\n")
         for row in path:
-            print(str(row)[1:-1])
+            f.write(str(row)[1:-1] + "\n")
+
+
+if __name__ == "__main__":
+    main()
